1b211d5e5e425cb15df511fa886db61bfd5b03af
Xiao <jcxsusan@gmail.com>
2021-02-21 16:12:05 +0100

    create custom hook for client to fetch data from sanity production

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,29 @@
 import React from 'react';
+import useLatestData from '../utils/useLatestData';
 
 export default function HomePage() {
-  return <div>hey!</div>;
+  const { sliceMasters, hotSlices } = useLatestData();
+
+  return (
+    <div className="center">
+      <h1>The Best Pizza Downtown!</h1>
+      <p>Open 11am to 11pm Every Single Day</p>
+      <div>
+        <CurrentlySlicing slicemasters={sliceMasters} />
+        <HotSlices hotSlices={hotSlices} />
+      </div>
+    </div>
+  );
 }
+
+const CurrentlySlicing = () => (
+  <div>
+    <p>CurrentlySlicing</p>
+  </div>
+);
+
+const HotSlices = () => (
+  <div>
+    <p>HotSlices</p>
+  </div>
+);
